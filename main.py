76e5b7b7610b592526467f72527c0d25d3a0a6f4
idian1 <108105770+idian1@users.noreply.github.com>
2022-06-23 22:39:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+c = 1
+n = int(input("ingrese un numero "))
+while c <= n:
+    p = c*2
+    print(p)
+    c = c+1
+
+input(" ")
+# Cuando el usuario ingresa el valor para la variable "n", se generea un ciclo regulado por la variable "c" la cual
+# aumentara en 1 con cada repeticion hasta alcanzar el valor de "n", ademas en cada repeticion imprimira la
+# variable "p" la cual es el resultado de el numero del ciclo actual multiplicado por 2 lo que dara como resultado
+# "n" numero de pares.
+# Este codigo utiliza el ciclo "while" por su simplisidad y facilidad de uso ademas se reutilizo el codigo de la
+# parte anterior modificando solo algunos pequeños detalles.
